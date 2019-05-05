d73f6f9b742cd1f71959512fc7c87ab7a5761f5c
Lucy20209060 <550502661@qq.com>
2019-05-06 00:40:28 +0800

    hello world

---
+++ demo-01/HelloWorld.java
@@ -0,0 +1,16 @@
+public class HelloWorld {
+    public static void main(String []args) {
+        System.out.println("Hello World"); // 打印 Hello World
+    }
+}
+
+/**
+ * 大小写敏感
+ * 类名首字母大写
+ * 方法名小写字母开头
+ * 所有的 Java 程序由 public static void main(String []args) 方法开始执行
+ * 
+ * 标志符
+ * 类名、变量名以及方法名都被称为标识符
+ * 所有的标识符都应该以字母（A-Z 或者 a-z）,美元符（$）、或者下划线（_）开始
+ */
