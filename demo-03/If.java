8311f752ac4e8ad6f1b03e812c77fc18d0541c63
Lucy20209060 <550502661@qq.com>
2019-05-21 23:27:36 +0800

    流程控制

---
+++ demo-03/If.java
@@ -0,0 +1,12 @@
+
+public class If {
+    public static void main(String[] args) {
+        // int a = 100;
+        // if(a == 100) {
+        //     System.out.println(a);
+        // }
+        for(int i=2; i<=100; i+=2) {
+            System.out.println(i);
+        }
+    }
+}
