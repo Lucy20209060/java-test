774b081923e66e3adfa891c666debab991de2c0e
Lucy20209060 <550502661@qq.com>
2019-06-02 15:48:32 +0800

    数据类型

--- demo-01/HelloWorld.java
+++ demo-01/HelloWorld.java
@@ -1,6 +1,16 @@
 public class HelloWorld {
     public static void main(String []args) {
-        System.out.println("Hello World"); // 打印 Hello World
+        // System.out.println("Hello World"); // 打印 Hello World
+        char a = 'A';
+        System.out.println(a);
+        char b = 65;
+        System.out.println(b);
+        char c = 65535;
+        System.out.println(c);
+        char d = (char)65536;
+        System.out.println(d);
+
+
     }
 }
 
