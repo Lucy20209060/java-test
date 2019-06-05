d9a1ff7595a64617196a047c1bafa8dbd786e60c
Lucy20209060 <550502661@qq.com>
2019-06-06 00:37:12 +0800

    包与类

---
+++ demo-04/CatTest.java
@@ -0,0 +1,8 @@
+public class CatTest {
+    public static void main(String[] args) {
+        Cat catTest = new Cat();
+        catTest.setName("小花");
+        String name = catTest.getName();
+        System.out.println(name);
+    }
+}
