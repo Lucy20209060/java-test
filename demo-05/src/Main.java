ad96e99322ca0583f5cb67ac50bd477e3f76065b
Lucy20209060 <550502661@qq.com>
2019-06-06 22:14:31 +0800

    包

---
+++ demo-05/src/Main.java
@@ -0,0 +1,9 @@
+import com.luchaoet.animal.*;
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        Cat myCat = new Cat();
+
+    }
+}
---
+++ demo-05/src/com/luchaoet/animal/Cat.java
@@ -0,0 +1,10 @@
+package com.luchaoet.animal;
+
+public class Cat {
+    public Cat() {
+        System.out.print("Cat构造函数");
+    }
+    public static void main(String[] args){
+        System.out.println(123);
+    }
+}
